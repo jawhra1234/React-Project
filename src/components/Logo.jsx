91b4fb4990cf0932d95d297227e095cb876dd7c9
jawhra1234 <shaikjawhra@gmail.com>
2025-07-04 17:00:34 +0530

    Updated project with Footer and other improvements

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,14 +1,10 @@
 
 import React from 'react'
 import { Link } from 'react-router-dom'
-function Logo({width = '100px'}) {
-  return (
-    <div>
-      <Link to="/" >
-        <img src="https://images.pexels.com/photos/13805647/pexels-photo-13805647.jpeg" alt="Logo" style={{ width }} className='rounded-full'/>
-      </Link>
-    </div>
-  )
+
+export default function Logo({ width }) {
+    return (
+        <img src="https://images.pexels.com/photos/3832354/pexels-photo-3832354.jpeg" alt="Logo" style={{ width }} className='rounded-full' />
+    )
 }
 
-export default Logo
